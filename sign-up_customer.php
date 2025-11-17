57c36493adf1197fd8c8f10e79ba2187d4de2b1a
Khaila <142780653+khai1a@users.noreply.github.com>
2025-11-17 13:42:35 +0800

    Update sign-up_customer.php

--- sign-up_customer.php
+++ sign-up_customer.php
@@ -78,7 +78,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="sign-up_customer.css">
+    <link rel="stylesheet" href="css/sign-up_customer.css">
 </head>
 
 <body>
@@ -184,3 +184,4 @@ window.onload = setCurrency;
 
 </body>
 </html>
+
