5404c0494503b2795271a076cc38d3c4d2fef38f
jmdelacuesta <janella_marie_delacuesta@dlsu.edu.ph>
2025-11-18 23:07:11 +0800

    Update manager_dashboard.php

--- BranchManager/manager_dashboard.php
+++ BranchManager/manager_dashboard.php
@@ -1,5 +1,7 @@
 <?php
-include('db_connect.php');
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+include('../db_connect.php');
 session_start();
 
 // block access if not logged in as a branch manager
