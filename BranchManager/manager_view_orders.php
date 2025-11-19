b6ce7bc8c4a7e6a05737c777acc7b4b7550ca941
jmdelacuesta <janella_marie_delacuesta@dlsu.edu.ph>
2025-11-19 22:25:56 +0800

    Update manager_view_orders.php

--- BranchManager/manager_view_orders.php
+++ BranchManager/manager_view_orders.php
@@ -85,11 +85,13 @@ $orders = array_values($orders);
 <div class="sidebar">
   <div class="sidebar-top"><h1>Aurum Scents</h1></div>
   <div class="sidebar-bottom">
-    <a href="manager_dashboard.php">Dashboard</a>
+        <a href="manager_dashboard.php">Dashboard</a>
     <a href="manager_inventory.php">Inventory</a>
-    <a href="manager_orders.php">Create Order</a>
+    <a href="manager_orders.php">Walk-In Orders</a>
     <a href="manager_returns.php">Returns</a>
-    <a href="manager_view_orders.php" class="active">View Orders</a>
+    <a href="manager_view_orders.php">View Orders</a>
+    <a href="sales_management.php">Sales Management</a> <!-- change to file name -->
+    <a href="staff_management.php">Staff Management</a>
   </div>
 </div>
 
