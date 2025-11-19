31cbd323d00ac56dbfdb4f48c78fc1720b90d39d
jmdelacuesta <janella_marie_delacuesta@dlsu.edu.ph>
2025-11-19 22:27:06 +0800

    Update sales_management.php

--- BranchManager/sales_management.php
+++ BranchManager/sales_management.php
@@ -174,13 +174,13 @@ $conn->next_result();
             <h1>Aurum Scents</h1>
         </div>
         <div class="sidebar-bottom">
-            <a href="manager_dashboard.php">Dashboard</a>
-            <a href="manager_inventory.php">Inventory</a>
-            <a href="manager_orders.php">Walk-In Orders</a>
-            <a href="manager_returns.php">Returns</a>
-            <a href="manager_view_orders.php">View Orders</a>
-            <a href="sales_management.php">Sales Management</a>
-            <a href="staff_management.php">Staff Management</a>
+                <a href="manager_dashboard.php">Dashboard</a>
+    <a href="manager_inventory.php">Inventory</a>
+    <a href="manager_orders.php">Walk-In Orders</a>
+    <a href="manager_returns.php">Returns</a>
+    <a href="manager_view_orders.php">View Orders</a>
+    <a href="sales_management.php">Sales Management</a> <!-- change to file name -->
+    <a href="staff_management.php">Staff Management</a>
         </div>
     </div>
 
@@ -303,3 +303,4 @@ $conn->next_result();
     </div>
 </body>
 </html>
+
