00109b75285e1ae0afecf5cbcf36d021000413f6
jmdelacuesta <janella_marie_delacuesta@dlsu.edu.ph>
2025-11-20 01:48:35 +0800

    Update customer_home.php

--- customer_home.php
+++ customer_home.php
@@ -9,7 +9,7 @@ require 'db_connect.php';
     <meta charset="UTF-8">
     <title>Aurum Scents</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/customer_home.css">
+    <link rel="stylesheet" href="customer_home.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
@@ -83,4 +83,5 @@ require 'db_connect.php';
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
 
