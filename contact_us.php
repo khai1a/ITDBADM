af2d8b4a7a233107b5061de15f10b833240dd185
jmdelacuesta <janella_marie_delacuesta@dlsu.edu.ph>
2025-11-20 21:32:36 +0800

    Update contact_us.php

--- contact_us.php
+++ contact_us.php
@@ -134,7 +134,7 @@ $conn->close();
 <meta charset="UTF-8">
 <title>Contact Us - Aurum Scents</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="css/contact_us.css">
+<link rel="stylesheet" href="contact_us.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
@@ -251,4 +251,5 @@ $conn->close();
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
 
