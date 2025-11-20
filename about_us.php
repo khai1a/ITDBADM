d5cce6b70063f60cfc7914dcba0567fda2cc4947
jmdelacuesta <janella_marie_delacuesta@dlsu.edu.ph>
2025-11-20 21:24:13 +0800

    Update about_us.php

--- about_us.php
+++ about_us.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>About Us - Aurum Scents</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/about_us.css">
+    <link rel="stylesheet" href="about_us.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
@@ -97,3 +97,4 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
